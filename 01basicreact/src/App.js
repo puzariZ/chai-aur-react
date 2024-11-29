adeaa30f302e52f630a4380c69adb1180f7b2703
AVINASH PANDEY <avinashpandey@AVINASHs-MacBook-Air.local>
2024-11-29 17:05:40 +0530

    1.1 commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Chai or react | Hi, I'm Avinash Pandey... in this react series i have 
+      learned about react basic version oif installation and vite app which is some small
+      in webpacks and have less dependcies and json like testing files and etc. to install 
+      basic reract app you need command npx create-basic-react 01basicreact and then npm run start and build
+      and whatever you want to perform.</h1>
   );
 }
 
