0e57ac08162ffbae87237bc1b271d5f2564ce3d8
AVINASH PANDEY <avinashpandey@AVINASHs-MacBook-Air.local>
2024-12-03 12:53:03 +0530

    commit 5.1 custom react@vite

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react'; // Import React (required for createElement)
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Visit the Google'
+);
+
+createRoot(document.getElementById('root')).render(reactElement);
