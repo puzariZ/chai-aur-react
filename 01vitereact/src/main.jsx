90db98ce14176d9e74173db34b6bb6f7d000622d
AVINASH PANDEY <avinashpandey@AVINASHs-MacBook-Air.local>
2024-12-03 12:54:29 +0530

    5.1.1 commit

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,30 @@ import React from 'react'; // Import React (required for createElement)
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+
+const reactElements = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'click me to visit google'
+}
+
+function MyApp() {
+  return(
+    <div>hi this is custom function !</div>
+  );
+}
+
+const anotherElement = (
+  <a href="https://google.com" target= "_blank">visit google veere </a>
+)
+
+// createRoot(document.getElementById('root')).render(
+//   anotherElement
+// )
+
 const reactElement = React.createElement(
   'a',
   { href: 'https://google.com', target: '_blank' },
