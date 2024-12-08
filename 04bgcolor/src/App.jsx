b63cb370cc1687df4a6cbb428b3491151f627f34
AVINASH PANDEY <avinashpandey@AVINASHs-MacBook-Air.local>
2024-12-08 13:35:58 +0530

    bg color-change

--- 04bgcolor/src/App.jsx
+++ 04bgcolor/src/App.jsx
@@ -25,6 +25,30 @@ function App() {
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "blue"}}
           >Blue</button>
+          <button onClick={() => setColor("olive")} 
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg" 
+          style={{backgroundColor: "olive"}}
+          >Olive</button>
+          <button onClick={() => setColor("gray")} 
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "gray"}}
+          >Gray</button>
+          <button onClick={() => setColor("yellow")} 
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "yellow"}}
+          >Yellow</button>
+          <button onClick={() => setColor("pink")} 
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "pink"}}
+          >Pink</button>
+          <button onClick={() => setColor("purple")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "purple"}}
+          >Purple</button>
+          <button onClick={() => setColor("lavender")}
+          className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+          style={{backgroundColor: "lavender"}}
+          >Lavender</button>
         </div>
       </div>
     </div>
